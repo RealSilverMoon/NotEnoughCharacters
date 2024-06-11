0d639e7e8a8c5ba8272ade89feb91c78d46ddfb0
RealSilverMoon <qazhhhh@outlook.com>
2024-06-11 17:00:36 +0800

    Support JEI Style Search Field

--- src/main/java/net/moecraft/nechar/NecharSearchProvider.java
+++ src/main/java/net/moecraft/nechar/NecharSearchProvider.java
@@ -2,15 +2,27 @@ package net.moecraft.nechar;
 
 import codechicken.nei.SearchField;
 import codechicken.nei.api.ItemFilter;
+import net.minecraft.util.EnumChatFormatting;
 
-public class NecharSearchProvider implements SearchField.ISearchProvider {
-    @Override
-    public boolean isPrimary() {
-        return false;
-    }
+public class NecharSearchProvider implements SearchField.ISearchParserProvider {
 
     @Override
     public ItemFilter getFilter(String searchText) {
         return new NecharSearchFilter(searchText);
+    }
+
+    @Override
+    public char getPrefix() {
+        return '*';
+    }
+
+    @Override
+    public EnumChatFormatting getHighlightedColor() {
+        return EnumChatFormatting.BLUE;
+    }
+
+    @Override
+    public SearchField.SearchMode getSearchMode() {
+        return SearchField.SearchMode.ALWAYS;
     }
 }
